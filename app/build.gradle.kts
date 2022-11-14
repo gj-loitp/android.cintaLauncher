06d993472e026f3a1b44d7d5e82c94bffbc77bc7
loitp <loitp@galaxy.one>
2022-11-14 14:40:21 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,21 +29,17 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.20")
     implementation("com.willowtreeapps:fuzzywuzzy-kotlin-jvm:0.9.0")
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-
+    implementation("com.github.bumptech.glide:glide:4.14.2")
     implementation("io.posidon:android.launcherUtils:30aa020c1a")
     implementation("io.posidon:android.libduckduckgo:22.0")
     implementation("io.posidon:android.rsslib:22.0")
     implementation("io.posidon:android.conveniencelib:22.0")
-
-    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.palette:palette-ktx:1.0.0")
-
-    implementation("com.google.android.material:material:1.5.0")
-
-    testImplementation("junit:junit:4.13.2")
+    implementation("com.google.android.material:material:1.7.0")
+//    testImplementation("junit:junit:4.13.2")
 }
 
 configurations.all {
