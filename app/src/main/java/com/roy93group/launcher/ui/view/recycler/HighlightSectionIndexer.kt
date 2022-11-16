a13e7b33ee6afa7efeeb3f9f5a5698d54ea1a855
loitp <loitp@galaxy.one>
2022-11-16 11:18:30 +0700

    up

--- app/src/main/java/com/roy93group/launcher/ui/view/recycler/HighlightSectionIndexer.kt
+++ app/src/main/java/com/roy93group/launcher/ui/view/recycler/HighlightSectionIndexer.kt
@@ -1,26 +1,16 @@
 package com.roy93group.launcher.ui.view.recycler
 
-import android.content.Context
-import android.graphics.drawable.ShapeDrawable
-import android.graphics.drawable.shapes.RoundRectShape
 import android.widget.SectionIndexer
-import com.roy93group.launcher.R
 import com.roy93group.launcher.data.items.App
-import io.posidon.android.conveniencelib.units.dp
-import io.posidon.android.conveniencelib.units.toFloatPixels
 
+/**
+ * Updated by Loitp on 2022.12.16
+ * Galaxy One company,
+ * Vietnam
+ * +840766040293
+ * freuss47@gmail.com
+ */
 interface HighlightSectionIndexer : SectionIndexer {
-
-    companion object {
-        fun createHighlightDrawable(context: Context): ShapeDrawable {
-            val bg = ShapeDrawable()
-            val r = 12.dp.toFloatPixels(context)
-            bg.shape = RoundRectShape(floatArrayOf(r, r, r, r, r, r, r, r), null, null)
-            bg.paint.color = context.getColor(R.color.white)
-            return bg
-        }
-    }
-
     fun highlight(i: Int)
     fun unhighlight()
     fun isDimmed(app: App): Boolean
