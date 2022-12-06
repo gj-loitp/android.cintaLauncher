3d198dcbf4c3fb48d5aa502edfe180888d571492
loitp <loitp@galaxy.one>
2022-12-06 10:32:59 +0700

    up

--- app/src/main/java/com/roy93group/launcher/ui/intro/FrmSplash.kt
+++ app/src/main/java/com/roy93group/launcher/ui/intro/FrmSplash.kt
@@ -1,5 +1,10 @@
 package com.roy93group.launcher.ui.intro
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import com.roy93group.app.C
 import com.roy93group.launcher.R
 
 /**
@@ -15,5 +20,19 @@ class FrmSplash : FrmWithNext(R.layout.frm_intro_splash) {
         isCheckedPolicy: Boolean
     ) {
         activity.setFragment(FrmPermissions())
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<AppCompatImageView>(R.id.ivLogo).apply {
+            setColorFilter(C.COLOR_PRIMARY_2)
+        }
+        view.findViewById<AppCompatTextView>(R.id.tv).apply {
+            setTextColor(C.COLOR_PRIMARY_2)
+        }
     }
 }
