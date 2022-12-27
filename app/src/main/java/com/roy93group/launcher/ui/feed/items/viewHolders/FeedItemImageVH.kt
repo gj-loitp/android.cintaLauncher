965f44408bf9238078edac686e683e0e06b1132a
loitp <loitp@galaxy.one>
2022-12-27 16:47:36 +0700

    up

--- app/src/main/java/com/roy93group/launcher/ui/feed/items/viewHolders/FeedItemImageVH.kt
+++ app/src/main/java/com/roy93group/launcher/ui/feed/items/viewHolders/FeedItemImageVH.kt
@@ -3,6 +3,7 @@ package com.roy93group.launcher.ui.feed.items.viewHolders
 import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -80,10 +81,15 @@ open class FeedItemImageVH(
     ) {
         super.onBind(item, isDisplayAppIcon)
         item as FeedItemWithBigImage
-        Glide.with(itemView.context)
-            .load(item.image)
-            .apply(requestOptions)
-            .listener(imageRequestListener)
-            .into(image)
+        if (isDisplayAppIcon) {
+            card.isVisible = true
+            Glide.with(itemView.context)
+                .load(item.image)
+                .apply(requestOptions)
+                .listener(imageRequestListener)
+                .into(image)
+        } else {
+            card.isVisible = false
+        }
     }
 }
