4d3de527edd9ffdce3f07b6cba81701245f73b71
loitp <loitp@galaxy.one>
2022-12-30 14:20:46 +0700

    up

--- app/src/main/java/com/roy93group/launcher/storage/ExternalStorage.kt
+++ app/src/main/java/com/roy93group/launcher/storage/ExternalStorage.kt
@@ -1,9 +1,7 @@
 package com.roy93group.launcher.storage
 
-import android.app.Activity
 import android.content.ContentValues
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
@@ -89,7 +87,7 @@ object ExternalStorage {
         }
     }
 
-    private const val FILE_REQUEST_CODE = 0xf113
+//    private const val FILE_REQUEST_CODE = 0xf113
 
 //    fun pickFile(activity: Activity, mime: String) {
 //        val intent = Intent()
