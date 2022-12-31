7a378e0dbd546d39c5c46565aee1a39c1265f41e
loitp <loitp@galaxy.one>
2022-12-31 16:22:58 +0700

    up

--- app/src/main/java/com/roy93group/lookerupper/ui/viewHolders/CompactSearchVH.kt
+++ app/src/main/java/com/roy93group/lookerupper/ui/viewHolders/CompactSearchVH.kt
@@ -3,16 +3,15 @@ package com.roy93group.lookerupper.ui.viewHolders
 import android.app.Activity
 import android.graphics.Color
 import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.isVisible
 import com.roy93group.app.C
-import com.roy93group.launcher.R
 import com.roy93group.launcher.providers.feed.suggestions.SuggestionsManager
 import com.roy93group.launcher.ui.feed.items.viewHolders.applyIfNotNull
 import com.roy93group.lookerupper.data.results.CompactAppResult
 import com.roy93group.lookerupper.data.results.CompactResult
 import com.roy93group.lookerupper.data.results.SearchResult
+import kotlinx.android.synthetic.main.view_search_result_compact.view.*
 
 /**
  * Updated by Loitp on 2022.12.18
@@ -25,14 +24,8 @@ class CompactSearchVH(
     itemView: View,
     val activity: Activity
 ) : SearchVH(itemView) {
-
-    val icon: ImageView = itemView.findViewById(R.id.icon)
-    val text: TextView = itemView.findViewById<TextView>(R.id.text).apply {
-        setTextColor(C.getColorPrimary())
-    }
-    private val subtitle: TextView = itemView.findViewById<TextView>(R.id.subtitle).apply {
-        setTextColor(C.getColorPrimary())
-    }
+    private val colorPrimary = C.getColorPrimary()
+//    private val colorBackground = C.getColorBackground()
 
     override fun onBind(
         result: SearchResult,
@@ -41,24 +34,36 @@ class CompactSearchVH(
     ) {
         result as CompactResult
 
-        icon.setImageDrawable(result.icon)
-        if (isDisplayAppIcon) {
-            icon.isVisible = true
-            if (isForceColorIcon) {
-                icon.setColorFilter(C.getColorPrimary())
-            } else {
-                icon.setColorFilter(Color.TRANSPARENT)
-            }
-        } else {
-            icon.isVisible = false
+        itemView.text.apply {
+            setTextColor(colorPrimary)
+            text = result.title
+        }
+        itemView.subtitle.apply {
+            setTextColor(colorPrimary)
+            applyIfNotNull(
+                view = itemView.subtitle,
+                value = result.subtitle,
+                block = TextView::setText
+            )
         }
 
-        text.text = result.title
-        applyIfNotNull(view = subtitle, value = result.subtitle, block = TextView::setText)
+        itemView.icon.apply {
+            setImageDrawable(result.icon)
+            if (isDisplayAppIcon) {
+                isVisible = true
+                if (isForceColorIcon) {
+                    setColorFilter(colorPrimary)
+                } else {
+                    setColorFilter(Color.TRANSPARENT)
+                }
+            } else {
+                isVisible = false
+            }
+        }
 
         itemView.setOnClickListener {
             if (result is CompactAppResult) {
-                SuggestionsManager.onItemOpened(it.context, result.app)
+                SuggestionsManager.onItemOpened(context = it.context, item = result.app)
             }
             result.open(it)
         }
