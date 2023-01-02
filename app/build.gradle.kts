45ec15a1957add560bcec6dc91051524776c2550
loitp <loitp@galaxy.one>
2023-01-02 13:59:21 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.tplloi:base:4.6.11")
+    implementation("com.github.tplloi:base:4.6.12")
     implementation("com.willowtreeapps:fuzzywuzzy-kotlin-jvm:0.9.0")
     implementation("io.posidon:android.launcherUtils:30aa020c1a")
     implementation("io.posidon:android.libduckduckgo:22.0")
