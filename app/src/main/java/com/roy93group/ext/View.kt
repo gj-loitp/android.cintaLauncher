c6b7e5b2995d740dd47d8f4220f2d4f4eb12108d
loitp <loitp@galaxy.one>
2023-01-08 19:15:22 +0700

    up

--- app/src/main/java/com/roy93group/ext/View.kt
+++ app/src/main/java/com/roy93group/ext/View.kt
@@ -1,9 +1,9 @@
 package com.roy93group.ext
 
 import com.google.android.material.card.MaterialCardView
+import com.loitp.core.ext.getDimenValue
 import com.loitp.core.ext.setCornerCardView
 import com.roy93group.launcher.R
-import kotlinx.android.synthetic.main.activity_search_search_bar.view.*
 
 /**
  * Created by Loitp on 08,January,2023
@@ -14,8 +14,8 @@ import kotlinx.android.synthetic.main.activity_search_search_bar.view.*
  */
 
 fun MaterialCardView.setCornerCardViewLauncher() {
-    val radiusTL = cardView.context.resources.getDimension(R.dimen.round_large)
-    val radiusTR = cardView.context.resources.getDimension(R.dimen.round_large)
+    val radiusTL = getDimenValue(R.dimen.round_large).toFloat()
+    val radiusTR = getDimenValue(R.dimen.round_large).toFloat()
     val radiusBL = 0f
     val radiusBR = 0f
     this.setCornerCardView(
