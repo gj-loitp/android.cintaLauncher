4f5d76799cf314219e128b47b1643be73ecb5be1
loitp <loitp@galaxy.one>
2023-03-03 18:27:37 +0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.gms:google-services:4.3.15")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
 
     }
 }
